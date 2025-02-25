d0d5b60f9e3f19e666ae3e56b5ed24d8721503d4
MaliSakshi07 <sakshimali467@gmail.com>
2025-02-25 22:03:06 +0530

    Pattern

---
+++ Pattern Printing Assignments/Assignment - Solutions/17 Pattern.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int r=0, c=0, i=0, j=0;
+
+    printf("\nEnter Row Size For Pattern :");
+    scanf("%d",&r);
+
+    printf("\nEnter Column Size For Pattern :");
+    scanf("%d",&c);
+
+    printf("\n");
+
+    for(i=1; i<=r; i++)
+    {
+        for(j=1; j<=c; j++)
+        {
+            if(i <= j)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
