edb39631b8c4ffc24debb8f676fa713156aa606d
MaliSakshi07 <sakshimali467@gmail.com>
2025-07-10 23:30:56 +0530

    Assignment

---
+++ Datatype Variable Assignment/04 Multipy two Numbers provided by user.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0;
+    int No2 = 0;
+    int Mult = 0;
+
+    printf("\nEnter 1st Number :");
+    scanf("%d",&No1);
+
+    printf("\nEnter 2nd Number :");
+    scanf("%d",&No2);
+
+    Mult = No1 * No2;
+
+    printf("\nMultiplication of %d and %d = %d",No1,No2,Mult);
+
+    getch();
+    return 0;
+}
