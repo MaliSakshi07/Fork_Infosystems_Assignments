6efc46ddc05e04ee91ba68dd5017b718ec946a04
MaliSakshi07 <sakshimali467@gmail.com>
2025-07-11 21:54:16 +0530

    Assignment

---
+++ Datatype Variable Assignment/06 Size of int, float,double.c.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\nSize of char : %u bytes",sizeof(char));
+    printf("\nSize Of int : %d bytes",sizeof(int));
+    printf("\nSize of double : %d bytes",sizeof(double));
+    printf("\nSize of float : %u bytes",sizeof(float));
+
+    getch();
+    return 0;
+}
