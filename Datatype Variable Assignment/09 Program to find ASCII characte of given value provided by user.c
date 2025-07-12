eea840e291c8e2be18d43a796b81f548e4ad8af2
MaliSakshi07 <sakshimali467@gmail.com>
2025-07-12 19:56:54 +0530

    Assignment

---
+++ Datatype Variable Assignment/09 Program to find ASCII characte of given value provided by user.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0;
+
+    printf("\nEnter a Number :");
+    scanf("%d",&i);
+
+    printf("\nASCII Character of given Value : %c",i);
+
+    getch();
+    return 0;
+}
