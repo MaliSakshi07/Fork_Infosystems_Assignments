4d74e735ea5e65451f972d21ab00630befd7dbcd
MaliSakshi07 <sakshimali467@gmail.com>
2025-07-13 13:09:09 +0530

    Assignment

---
+++ Datatype Variable Assignment/14 Accept Side and Calculate Area of Square.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    float side = 0.0;
+    float Area = 0.0;
+
+    printf("\nEnter Side for Square :");
+    scanf("%f",&de);
+
+
+    Area = side * side;
+
+    printf("\nArea of Square : %f",Area);
+
+    getch();
+    return 0;
+}
+
