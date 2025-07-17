377fd4875aca98577778280fda7c0aa8f2f6589c
MaliSakshi07 <sakshimali467@gmail.com>
2025-07-17 21:56:49 +0530

    Assignment

---
+++ Assignment - 2 Selection Statement/Assignment - Solution/03 program to find the maximum number between two given numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0, No2 = 0;
+
+    printf("\nEnter 1st Number :");
+    scanf("%d",&No1);
+    printf("\nEnter 2nd Number :");
+    scanf("%d",&No2);
+
+    if(No1 > No2)
+    {
+        printf("\nGiven %d Number is Greater.",No1);
+    }
+    else if(No1 < No2)
+    {
+        printf("\nGiven %d Number is Greater.",No2);
+    }
+
+    getch();
+    return 0;
+}
+
