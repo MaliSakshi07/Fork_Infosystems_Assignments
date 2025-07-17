29ec546881c3265a199441dbab070bf8de210809
MaliSakshi07 <sakshimali467@gmail.com>
2025-07-17 21:56:12 +0530

    Assignment

---
+++ Assignment - 2 Selection Statement/Assignment - Solution/02 program to check if a given number is positive or negative.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0;
+
+    printf("\nEnter a Number :");
+    scanf("%d",&No);
+
+    if(No > 0)
+    {
+        printf("\nGiven %d Number is Positive.",No);
+    }
+    else
+    {
+        printf("\nGiven %d Number is Negative.",No);
+    }
+
+    getch();
+    return 0;
+}
