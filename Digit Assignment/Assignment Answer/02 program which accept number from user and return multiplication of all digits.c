7f7b8a345adc3f02cb513d34ea2aaccd3a02800d
MaliSakshi07 <sakshimali467@gmail.com>
2025-07-30 21:56:18 +0530

    Assignment

---
+++ Digit Assignment/Assignment Answer/02 program which accept number from user and return multiplication of all digits.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0, mult = 1, rem = 0;
+
+    printf("\nEnter a Number :");
+    scanf("%d",&No);
+
+    while(No != 0)
+    {
+        rem = No%10;
+        mult = mult * rem;
+        No /= 10;
+    }
+    printf("\nMultiplication of Number is : %d",mult);
+
+    getch();
+    return 0;
+}
