7e4b1ecb539a4c1b1e3a50946948d9bba6a6cfe2
MaliSakshi07 <sakshimali467@gmail.com>
2025-07-30 21:55:08 +0530

    Assignment

---
+++ Digit Assignment/Assignment Answer/01 program that accepts a number from the user, sums its digits, and displays them in the following format.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0, sum = 0, i = 0;
+
+    printf("\nEnter a Number :");
+    scanf("%d",&No);
+
+    while(No != 0)
+    {
+        i = No % 10;
+        sum = i + sum;
+        No /= 10;
+    }
+    printf("Sum = %d ",sum);
+
+    getch();
+    return 0;
+}
