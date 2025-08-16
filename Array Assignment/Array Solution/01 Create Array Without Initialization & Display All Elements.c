36604e66528cbf48119cd77af0bf4a86bec10624
MaliSakshi07 <sakshimali467@gmail.com>
2025-08-16 22:20:40 +0530

    Assignment

---
+++ Array Assignment/Array Solution/01 Create Array Without Initialization & Display All Elements.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int arr[5];
+
+    printf("\nElements of Array :\n");
+    printf("\nvalue of 1st element : %d",arr[0]);
+    printf("\nvalue of 2nd element : %d",arr[1]);
+    printf("\nvalue of 3rd element : %d",arr[2]);
+    printf("\nvalue of 4th element : %d",arr[3]);
+    printf("\nvalue of 5th element : %d",arr[4]);
+
+    getch();
+    return 0;
+
+}
