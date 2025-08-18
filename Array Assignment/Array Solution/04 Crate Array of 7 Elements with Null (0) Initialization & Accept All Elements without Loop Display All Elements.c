8153675dfd3a29db373110cfaae86e9e3d7e873f
MaliSakshi07 <sakshimali467@gmail.com>
2025-08-18 21:43:44 +0530

    Assignment

---
+++ Array Assignment/Array Solution/04 Crate Array of 7 Elements with Null (0) Initialization & Accept All Elements without Loop Display All Elements.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int arr[7] = {};
+
+    printf("\nElements of Array :\n");
+    printf("\nValue of 1st element : %d",arr[0]);
+    printf("\nValue of 2nd element : %d",arr[1]);
+    printf("\nValue of 3rd element : %d",arr[2]);
+    printf("\nValue of 4th element : %d",arr[3]);
+    printf("\nValue of 5th element : %d",arr[4]);
+    printf("\nValue of 6th element : %d",arr[5]);
+    printf("\nValue of 7th element : %d",arr[6]);
+
+    getch();
+    return 0;
+}
+
