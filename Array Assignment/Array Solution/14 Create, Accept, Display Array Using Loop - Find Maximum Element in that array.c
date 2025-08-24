be6d8b8b3c42a71b92f3f2f9eef13b0adf5a8c98
MaliSakshi07 <sakshimali467@gmail.com>
2025-08-24 18:17:42 +0530

    Assignment

---
+++ Array Assignment/Array Solution/14 Create, Accept, Display Array Using Loop - Find Maximum Element in that array.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+#define Size 5
+
+int main()
+{
+    int Arr[Size] = {},i=0,Max = 0;
+
+    printf("\nAccepting Element Of Array :");
+    for(i=0;i<Size;i++)
+    {
+        printf("\nEnter Element %d :",i+1);
+        scanf("%d",&Arr[i]);
+    }
+
+    printf("\nDisplay Array Element:\n");
+    for(i=0; i<Size; i++)
+    {
+        printf("\nArray Element %d : %d",i+1,Arr[i]);
+    }
+
+    for(i=0; i < Size; i++)
+    {
+        if(Max <= Arr[i])
+        {
+            Max = Arr[i];
+        }
+    }
+    printf("\nMaximum Element of An Array is : %d",Max);
+
+    getch();
+    return 0;
+}
