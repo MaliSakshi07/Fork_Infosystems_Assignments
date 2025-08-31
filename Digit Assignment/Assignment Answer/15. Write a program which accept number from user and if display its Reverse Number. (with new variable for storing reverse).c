8e14a3163af895382e334f3e9e31f54b4ce58dbc
MaliSakshi07 <sakshimali467@gmail.com>
2025-08-31 21:21:47 +0530

    Assignment

---
+++ Digit Assignment/Assignment Answer/15. Write a program which accept number from user and if display its Reverse Number. (with new variable for storing reverse).c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int no = 0, temp = 0, rev = 0,rem = 0;
+
+    printf("\nEnter a Number :");
+    scanf("%d",&no);
+
+    temp = no;
+    while(temp != 0)
+    {
+        rem = temp%10;
+        rev = (rev*10)+rem;
+        temp /=10;
+    }
+    printf("Reverse Number is : %d",rev);
+
+    getch();
+    return 0;
+}
