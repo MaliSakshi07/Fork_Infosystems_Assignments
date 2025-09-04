8b7ab0f8d3e3a027e43e42bd2bce70d69cb6f3de
MaliSakshi07 <sakshimali467@gmail.com>
2025-09-04 22:24:20 +0530

    Assignment

---
+++ Assignment - 2 Selection Statement/Assignment - Solution/17 program to accept a character from user and convert case (Toggle Case) of that character.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch;
+
+    printf("\nEnter a Character : ");
+    scanf("%c",&ch);
+
+    if(ch >= 'A' && ch <= 'Z')
+    {
+        printf("")
+    }
+}
